bc5964f5696876cd57314563e858e4b4f7f1ee45
Utshah Neupane <utshah777@gmail.com>
2024-01-29 22:29:44 -0600

    three and done

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -11,17 +11,21 @@ public class Note{
 	}
 
 	public Note (Pitch pitch, int octave){
-	this.pitch = pitch;
+		this.pitch = pitch;
 
-	//validating octave
-	if (octave < -5) this.octave = -5;
-	else if (octave >= -5 && octave <=4) this.octave = octave;
-	else this.octave = 4;
+		//validating octave
+		if (octave < -5) this.octave = -5;
+		else if (octave >= -5 && octave <=4) this.octave = octave;
+		else this.octave = 4;
 	}
 
 	//One public Method
 
+	@Override
+	public String toString(){
+		if (pitch == null) return " ";
 
-	
-
+		if (octave != 0) return (pitch.toString() + Integer.toString(octave));
+		else return pitch.toString();
+	}
 }
