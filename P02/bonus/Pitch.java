1ecbee060961bc9a0bc5465bd84ef002926738cc
Utshah Neupane <utshah777@gmail.com>
2024-01-30 07:02:33 -0600

    two and done

--- P02/bonus/Pitch.java
+++ P02/bonus/Pitch.java
@@ -1,1 +1,21 @@
-public enum Pitch {C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B}
+public enum Pitch{
+	C("C"), Db("Db"), D("D"), Eb("Eb"), E("E"), F("F"), Gb("Gb"), G("G"), Ab("Ab"), A("A"), Bb("Bb"), B("B");
+
+	private final String string;
+
+	private Pitch(String string){
+		this.string = string;
+	}
+
+	@Override
+	public String toString(){
+		switch (this){
+			case Db: return "D\u266d";
+			case Eb: return "E\u266d";
+			case Gb: return "G\u266d";
+			case Ab: return "A\u266d";
+			case Bb: return "B\u266d";
+			default: return name();
+		}
+	}
+}
