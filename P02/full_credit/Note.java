94307fe067cb7c4cc1832801740ecc5f56478e8f
Utshah Neupane <utshah777@gmail.com>
2024-01-29 22:27:33 -0600

    two and done

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -1,10 +1,25 @@
 public class Note{
+
+	//Two private Felds
 	private Pitch pitch;
 	private int octave;
 
+
+	//Two public Constructors
 	public Note(){
 		this.pitch = null;
 	}
+
+	public Note (Pitch pitch, int octave){
+	this.pitch = pitch;
+
+	//validating octave
+	if (octave < -5) this.octave = -5;
+	else if (octave >= -5 && octave <=4) this.octave = octave;
+	else this.octave = 4;
+	}
+
+	//One public Method
 
 
 	
