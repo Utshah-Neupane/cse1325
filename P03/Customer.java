6cf2c182b00a92a5318757db87c92434e0eea525
Utshah Neupane <utshah777@gmail.com>
2024-02-05 23:32:10 -0600

    one and done

---
+++ P03/Customer.java
@@ -0,0 +1,28 @@
+public class Customer{
+	private String name;
+	private String email;
+
+
+
+
+	public Customer (String name, String email){
+
+	}
+
+	@Override
+	public toString (){
+		
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+
