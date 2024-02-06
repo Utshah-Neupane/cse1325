8ad3dd063bf92727e6d6487645b021237ca26a26
Utshah Neupane <utshah777@gmail.com>
2024-02-06 07:28:28 -0600

    one and done

---
+++ P03/TestCustomer.java
@@ -0,0 +1,39 @@
+public class TestCustomer{
+	public static void main (String[] args){
+		int error_count = 0;
+
+
+		Customer customer1 = new Customer("Nancy", "nancy_white@gmail.com");
+		
+		
+
+		if (!customer1.toString().equals("Nancy(nancy_white@gmail.com)"){
+			System.err.println("ERROR: Incorrect string "+ customer1.toString() + " (Nancy(nancy_white@gmail.com) expected)";
+			error_count++;
+		}
+
+
+
+		try {
+			Customer customer2 = new Customer("Alicia", "alicia.green-gmail.com");	
+			System.err.println ("ERROR: No exception thrown for a missing '@'");
+			error_count++;
+		}
+			catch (Exception e){
+			}
+
+
+		try {
+			Customer customer3 = new Customer("Roy", "roy.yellow@gmailcom");	
+			System.err.println ("ERROR: No exception thrown for a missing '.' after '@'");
+			error_count++;
+		}
+			catch (Exception e){
+			}
+
+
+
+		System.exit (error_count);
+
+	}
+}
