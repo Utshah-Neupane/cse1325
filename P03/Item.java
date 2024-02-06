5be78955f102ee7a004889b438d98c1f920c64cc
Utshah Neupane <utshah777@gmail.com>
2024-02-06 00:29:43 -0600

    one and done

---
+++ P03/Item.java
@@ -0,0 +1,27 @@
+public class Item{
+
+	private Product product;
+	private int quantity;
+
+	public Item (Product product, int quantity){
+
+	}
+
+	public int getPrice(){
+
+	}
+
+	@Override
+	public String toString(){
+		
+	}
+
+
+
+
+
+
+}
+
+
+
