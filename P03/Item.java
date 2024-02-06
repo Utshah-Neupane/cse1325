096149b8ee9fef1a83dea4080fa69cd7e02a60df
Utshah Neupane <utshah777@gmail.com>
2024-02-06 06:20:31 -0600

    two and done

--- P03/Item.java
+++ P03/Item.java
@@ -4,16 +4,22 @@ public class Item{
 	private int quantity;
 
 	public Item (Product product, int quantity){
-
+		this.product = product;
+		this.quantity = quantity;
 	}
 
 	public int getPrice(){
+		return (quantity * product.getPrice());
 
 	}
 
 	@Override
 	public String toString(){
-		
+		int dollar = price / 100;
+		int cents = price % 100;
+
+		return String.format("%-30s $ %d.%02d", name, dollar, cents);
+
 	}
 
 
