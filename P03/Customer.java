efd8340bbb0a1fa368c925e2314ea5e4b685c4cc
Utshah Neupane <utshah777@gmail.com>
2024-02-05 23:50:32 -0600

    two and done

--- P03/Customer.java
+++ P03/Customer.java
@@ -1,25 +1,22 @@
 public class Customer{
+
 	private String name;
 	private String email;
 
 
-
-
 	public Customer (String name, String email){
+		if (!email.contains("@)") || email.indexOf("@") > email.lastIndexOf(".")){
+			throw new IllegalArgumentException ("Invalid Email Address: " + email);
+		}
 
+		this.name = name;
+		this.email = email;
 	}
 
 	@Override
 	public toString (){
-		
+		return (name + "(" + email + ")");
 	}
-
-
-
-
-
-
-
 }
 
 
