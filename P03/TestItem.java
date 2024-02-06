7dddaea9dd776bd648cfa502b25533d1ed843344
Utshah Neupane <utshah777@gmail.com>
2024-02-06 07:44:49 -0600

    two and done

--- P03/TestItem.java
+++ P03/TestItem.java
@@ -8,29 +8,14 @@ public class TestItem{
 		Product product2 = new Product("Apple", 1499);
 		Item item2 = new Item(product2, 12);
 		
-		if (!customer1.toString().equals("Nancy(nancy_white@gmail.com)")){
-			System.err.println("ERROR: Incorrect string "+ customer1.toString() + " (Nancy(nancy_white@gmail.com) expected)");
+		if (item1.getPrice() != 4198){
+			System.err.println("ERROR: Incorrect price "+ item1.getPrice() + " (4198 expected)");
 			error_count++;
 		}
 
 
 
-		try {
-			Customer customer2 = new Customer("Alicia", "alicia.green-gmail.com");	
-			System.err.println ("ERROR: No exception thrown for a missing '@'");
-			error_count++;
-		}
-			catch (Exception e){
-			}
-
-
-		try {
-			Customer customer3 = new Customer("Roy", "roy.yellow@gmailcom");	
-			System.err.println ("ERROR: No exception thrown for a missing '.' after '@'");
-			error_count++;
-		}
-			catch (Exception e){
-			}
+		
 
 
 		System.exit (error_count);
