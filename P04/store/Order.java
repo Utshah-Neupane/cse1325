115d99c518a7ffe672413e3a65f6cc3702933380
Utshah Neupane <utshah777@gmail.com>
2024-02-13 06:56:57 -0600

    two and done

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -33,7 +33,20 @@ public class Order{
 
 	@Override
 	public String toString(){
-		
+		StringBuilder receipt = new StringBuilder();
+		receipt.append("Order #" + orderNumber + "for " + customer);
+
+		for (Item item: items){
+			receipt.append(item);
+			System.out.println();
+		}
+
+		int dollar = getPrice() / 100;
+		int cents = getPrice() % 100;
+
+		receipt.append("Order Total: $").append(dollar).append(".").append(cents);
+		return receipt.toString();
+
 	}
 
 
