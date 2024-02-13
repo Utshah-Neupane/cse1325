4dd7a67e700343c9d9ae2d5c8693f87f3b91120c
Utshah Neupane <utshah777@gmail.com>
2024-02-13 04:33:02 -0600

    one and done

---
+++ P04/store/Product.java
@@ -0,0 +1,42 @@
+package store; 
+
+public class Product{
+
+	private static int nextStockNumber = 0;
+	private int stockNumber;
+	private String name;
+	private int price;
+
+
+	public Product(String name, int price){
+		if (price < 0){
+			throw new IllegalArgumentException ("Invalid price of " + name + " : " + price);
+		}
+
+		this.stockNumber = nextStockNumber++;
+		this.name = name;
+		this.price = price;
+	}
+
+
+	public int getStockNumber(){
+		return stockNumber;
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+
+	@Override
+	public String toString(){
+		int dollar = price / 100;
+		int cents = price % 100;
+
+		return String.format("%-30s $ %d.%02d", name, dollar, cents);
+	}
+
+}
+
+
+
