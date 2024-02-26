fd806911ac33ce708612f6f25fab6321ff75d628
Utshah Neupane <utshah777@gmail.com>
2024-02-26 15:23:17 -0600

    one and done

---
+++ P05/store/Store.java
@@ -0,0 +1,44 @@
+package store;
+
+public class Store{
+	private String name;
+	private Customer[] customers;
+	private Product[] products;
+	private Order[] orders;
+
+	public Store(String name){
+	}
+
+	public String getName(){
+	}
+
+
+
+	public void addCustomer(Customer customer){
+	}
+
+
+	public String getCustomerList(){
+	}
+
+
+	public void addProduct(Product product){
+	}
+
+
+	public String getProductList(){
+	}
+
+
+
+
+	public int 
+
+
+
+
+
+}
+
+
+
