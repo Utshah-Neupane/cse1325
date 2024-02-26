95c23a7ac41041ae3f6b58c35a80b6ac3a5e9b01
Utshah Neupane <utshah777@gmail.com>
2024-02-26 16:22:45 -0600

    four and done

--- P05/store/Store.java
+++ P05/store/Store.java
@@ -25,41 +25,53 @@ public class Store{
 		customers.add(customer);
 	}
 
-
 	public String getCustomerList(){	
 		StringBuilder menu = new StringBuilder();
-		for (int i = 0; i < customers.size(), i++){
-			menu.append(i).append("]").append(customers.get(i).append("\n"));
+		for (int i = 0; i < customers.size(); i++){
+			menu.append(i).append("] ").append(customers.get(i)).append("\n");
 		}
 		return menu.toString();
 	}
 
 
+
 	public void addProduct(Product product){
+		products.add(product);
 	}
 
-
 	public String getProductList(){
+		StringBuilder menu = new StringBuilder();
+		for (int i = 0; i < products.size(); i++){
+			menu.append(i).append("] ").append(products.get(i)).append("\n");
+		}
+		return menu.toString();
 	}
 
 
 
 
 	public int newOrder(int customerIndex){
+		Customer customer = customers.get(customerIndex);
+		Order order = new Order(customer);
+		orders.add(order);
+		return orders.indexOf(order);
 	}
 
 
-	public void addToOrder(int orderIndex, int productIndex, int quantiy){
+	public void addToOrder(int orderIndex, int productIndex, int quantity){
+		Product product = products.get(productIndex);
+		Item item = new Item(product,quantity);
+		orders.get(orderIndex).addItem(item);
 	}
 
 
 	public String getOrderList(){
+		StringBuilder menu = new StringBuilder();
+		for (int i = 0; i < orders.size(); i++){
+			menu.append(i).append("] ").append(orders.get(i)).append("\n");
+		}
+		return menu.toString();
 	}
-
-
-
-
-
 }
 
 
