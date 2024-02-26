cc14b4f6357d3cdde9361de365c3dac53521a457
Utshah Neupane <utshah777@gmail.com>
2024-02-26 15:53:07 -0600

    three and done x

--- P05/store/Store.java
+++ P05/store/Store.java
@@ -1,24 +1,37 @@
 package store;
+import java.util.ArrayList;
 
 public class Store{
 	private String name;
-	private Customer[] customers;
-	private Product[] products;
-	private Order[] orders;
+	private ArrayList<Customer> customers;
+	private ArrayList<Product> products;
+	private ArrayList<Order> orders;
+
 
 	public Store(String name){
+		this.name = name;
+		this.customers = new ArrayList<>();
+		this.products = new ArrayList<>();
+		this.orders = new ArrayList<>();
 	}
 
 	public String getName(){
+		return name;
 	}
 
 
 
 	public void addCustomer(Customer customer){
+		customers.add(customer);
 	}
 
 
-	public String getCustomerList(){
+	public String getCustomerList(){	
+		StringBuilder menu = new StringBuilder();
+		for (int i = 0; i < customers.size(), i++){
+			menu.append(i).append("]").append(customers.get(i).append("\n"));
+		}
+		return menu.toString();
 	}
 
 
